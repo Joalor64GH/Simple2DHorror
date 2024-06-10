cdba2d520c8e11819f5db20638c95a69c9d0fb2e
Joalor64 <rhgarcon@gmail.com>
2024-06-10 09:34:57 -0400

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -64,7 +64,7 @@ class PlayState extends FlxState
 	{
 		return (sprite1.x < sprite2.x + sprite2.width && 
 			sprite1.x + sprite1.width > sprite2.x &&
-			sprite1.y < + sprite2.y + sprite2.height && 
+			sprite1.y < sprite2.y + sprite2.height && 
 			sprite1.y + sprite1.height > sprite2.y);
 	}
 }
